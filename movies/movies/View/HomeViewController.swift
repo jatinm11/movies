5ee89976c4e5cca069e80344a4e98b6e7ca70470
Jatin Menghani <jatin-pc@Jatins-MacBook-Pro-2.local>
2019-04-12 02:17:08 +0530

    Added basic home and search view controllers

---
+++ movies/movies/View/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  movies
+//
+//  Created by Jatin Menghani on 12/04/19.
+//  Copyright © 2019 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
