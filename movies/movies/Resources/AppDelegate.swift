c745864618af09a0bb73e2be946e54a6b1deec35
Jatin Menghani <jatin-pc@Jatins-MacBook-Pro-2.local>
2019-04-12 01:34:30 +0530

    Re-organize project

---
+++ movies/movies/Resources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  movies
+//
+//  Created by Jatin Menghani on 12/04/19.
+//  Copyright © 2019 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
