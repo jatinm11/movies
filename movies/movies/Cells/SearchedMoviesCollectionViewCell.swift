1baa93fb0ee8eafd67fb3841f486aa4155b445bf
Jatin Menghani <jatin-pc@Jatins-MacBook-Pro-2.local>
2019-04-12 20:10:50 +0530

    Added basic UI in search view contoller

---
+++ movies/movies/Cells/SearchedMoviesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SearchedMoviesCollectionViewCell.swift
+//  movies
+//
+//  Created by Jatin Menghani on 12/04/19.
+//  Copyright © 2019 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+class SearchedMoviesCollectionViewCell: UICollectionViewCell {
+    
+}
--- movies/movies/View/SearchViewController.swift
+++ movies/movies/View/SearchViewController.swift
@@ -9,22 +9,14 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
