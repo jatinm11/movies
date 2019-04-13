1da33d8ef965a637a06b9cea4e616aa5b5c8d535
Jatin Menghani <jatin-pc@Jatins-MacBook-Pro-2.local>
2019-04-14 02:19:12 +0530

    Initialized empty MovieDetails class and view controller(VC)

---
+++ movies/movies/View/MovieDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsViewController.swift
+//  movies
+//
+//  Created by Jatin Menghani on 14/04/19.
+//  Copyright © 2019 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
